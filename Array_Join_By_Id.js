1d75ae8ac368f12113e7d3caa9f4b0c8a5df8aa0
Harikrishnan-K <krishnakripaharikrishnan@gmail.com>
2024-10-28 09:37:50 +0530

    array join by id

--- Array_Join_By_Id.js
+++ Array_Join_By_Id.js
@@ -1,13 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Joining Array By Id</title>
-</head>
-<body>
-    <script>
-
 // Given two arrays arr1 and arr2, return a new array joinedArray. All the objects in each of the two inputs arrays will contain an id field that has an integer value. 
 
 // joinedArray is an array formed by merging arr1 and arr2 based on their id key. The length of joinedArray should be the length of unique values of id. The returned array should be sorted in ascending order based on the id key.
@@ -53,7 +43,3 @@ const arr2 = [
 ];
 
 console.log(join(arr1, arr2));
-
-    </script>
-</body>
-</html>
