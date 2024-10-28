a795dbaf9d10f7a5a1a170081c34f8de1ebabc30
Harikrishnan-K <krishnakripaharikrishnan@gmail.com>
2024-10-28 09:34:43 +0530

    add promise

--- Add_Two_Promise.js
+++ Add_Two_Promise.js
@@ -1,13 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Add Two Promise</title>
-</head>
-<body>
-    <script>
-
 // Given two promises promise1 and promise2, return a new promise. promise1 and promise2 will both resolve with a number. The returned promise should resolve with the sum of the two numbers.
 
 var addTwoPromises = async function(promise1, promise2)
@@ -18,7 +8,3 @@ return result1 + result2;
 
 addTwoPromises(Promise.resolve(5), Promise.resolve(5))
 .then(console.log);
-
-    </script>
-</body>
-</html>
