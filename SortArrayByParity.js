0d38f60cd41e28984aad497fce4236b174671b7d
Harikrishnan-K <krishnakripaharikrishnan@gmail.com>
2024-11-06 19:27:40 +0530

    sort array by parity

---
+++ SortArrayByParity.js
@@ -0,0 +1,9 @@
+var sortArrayByParity = function(nums) {
+    const even = nums.filter(nums => nums % 2 === 0)
+    const odd = nums.filter(nums => nums % 2 !== 0)
+    const result = even.concat(odd);
+    return result;
+    
+};
+console.log(sortArrayByParity([3,1,2,4]));
+console.log(sortArrayByParity([3,5,8,7,1,2,4]));
