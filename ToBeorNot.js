898963e653863622afda5e900a04e7da38a3043b
Harikrishnan-K <krishnakripaharikrishnan@gmail.com>
2024-11-12 09:47:07 +0530

    to be or not

--- ToBeorNot.js
+++ ToBeorNot.js
@@ -1,12 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title> To Be Or Not To Be</title>
-</head>
-<body>
-    <script>
+
 // Write a function expect that helps developers test their code. It should take in any value val and return an object with the following two functions.
 
 // toBe(val) accepts another value and returns true if the two values === each other. If they are not equal, it should throw an error "Not Equal".
@@ -29,8 +21,3 @@
 
  console.log(expect(5).toBe(5));
  console.log(expect(5).notToBe(5)); 
-
-
-    </script>
-</body>
-</html>
