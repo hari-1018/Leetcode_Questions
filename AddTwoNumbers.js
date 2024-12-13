d9e6b6b14712c0b76e4ffa74395441d3314558b1
Harikrishnan-K <krishnakripaharikrishnan@gmail.com>
2024-12-13 11:04:32 +0530

    add two numbers

---
+++ AddTwoNumbers.js
@@ -0,0 +1,18 @@
+// Given two integers num1 and num2, return the sum of the two integers.
+ 
+// Example 1:
+// Input: num1 = 12, num2 = 5
+// Output: 17
+
+// Example 2:
+// Input: num1 = -10, num2 = 4
+// Output: -6
+
+var sum = function(num1, num2){
+    let sum = num1 + num2;
+    return sum;
+}
+
+console.log(sum(12, 5));
+console.log(sum(-10, 4));
+
