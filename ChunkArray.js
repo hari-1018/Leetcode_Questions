addde36da725aa9b9529c367d283f4f2cc45a3b9
Harikrishnan-K <krishnakripaharikrishnan@gmail.com>
2024-12-25 10:19:20 +0530

    chunk array

--- ChunkArray.js
+++ ChunkArray.js
@@ -1,20 +1,18 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Chunk Array</title>
-</head>
-<body>
-    <script>
-
 // Given an array arr and a chunk size size, return a chunked array.
 
 // A chunked array contains the original elements in arr, but consists of subarrays each of length size. The length of the last subarray may be less than size if arr.length is not evenly divisible by size.
 
+// Example 1:
+// Input: arr = [1,2,3,4,5], size = 1
+// Output: [[1],[2],[3],[4],[5]]
+
+// Example 2:
+// Input: arr = [1,9,6,3,2], size = 3
+// Output: [[1,9,6],[3,2]]
+
         const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
         const size = 2;
-        var chunk = function(arr, size) {
+        var chunkArray = function(arr, size) {
             if(arr.length===0)
             return []; 
 
@@ -24,8 +22,4 @@
             }
             return result;
         };
-        console.log(chunk(arr, size));
-
-    </script>
-</body>
-</html>
+        console.log(chunkArray(arr, size));
